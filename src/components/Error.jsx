d1c617df454c297b69fd7891ae245514a7e142be
Dark <redwan.rahat69@gmail.com>
2024-04-24 10:22:21 +0600

    minor bug fixes and responsive

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,14 +1,22 @@
 import { useNavigate } from "react-router-dom";
 
 const Error = () => {
-    const  nav = useNavigate()
-    return (
-
-        <div className="h-lvh items-center text-center mt-8 ">
-            <h1 className="text-7xl text-black">OOPS!!! Something Went Wrong</h1>
-            <button onClick={()=>{nav(-1)}} className="px-4 py-3 rounded-lg bg-common mt-20 text-2xl text-white font-bold">Go Back</button>
-        </div>
-    );
+  const nav = useNavigate();
+  return (
+    <div className="h-lvh items-center text-center  bg-gray-200">
+      <h1 className="pt-8 md:text-7xl text-black">
+        OOPS!!! Something Went Wrong
+      </h1>
+      <button
+        onClick={() => {
+          nav(-1);
+        }}
+        className="px-4 md:py-3 py-1 rounded-md md:rounded-lg bg-common mt-4 md:mt-20 md:text-2xl text-sm text-white font-semibold md:font-bold"
+      >
+        Go Back
+      </button>
+    </div>
+  );
 };
 
 export default Error;
